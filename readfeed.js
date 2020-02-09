a13f298bf0a243e4026ffc7844c1cc06cc422768
developer1 <nianjie@hotmail.co.jp>
2020-02-09 20:17:21 +0900

    use built-in object of Promise to wrap q-promise.

--- readfeed.js
+++ readfeed.js
@@ -5,22 +5,20 @@ const parser = new xml2js.Parser();
 
 const readFeed = {
   readFrom(feedurl) {
-    return HTTP.request(feedurl)
+    return new Promise((resolve, reject) => {
+      HTTP.request(feedurl)
       .then(res => res.body.read())
-      // Here the parser, a dependent 3rd lib, makes use of the Node.js callback pattern,
-      // where callbacks are in the form of function(err, result).
-      // Wrapping as thenable so make it support promise.
-      .then(body => Promise.resolve({
-        then(onFullfill, onReject) {
-          parser.parseString(body, (error, result) => {
-            if (error) {
-              onReject(error);
-            } else {
-              onFullfill(result);
-            }
-          });
-        },
-      }));
+      .then(body => {
+        parser.parseString(body, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        })
+      })
+      .catch(reason => reject(reason))
+    });
   },
 };
 
