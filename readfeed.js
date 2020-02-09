9b9855ab4e817436648ae7e26ffbfa6fa8fcaf37
developer1 <nianjie@hotmail.co.jp>
2020-02-09 20:47:40 +0900

    append comment.

--- readfeed.js
+++ readfeed.js
@@ -5,19 +5,22 @@ const parser = new xml2js.Parser();
 
 const readFeed = {
   readFrom(feedurl) {
+    // Here the parser, a dependent 3rd lib, makes use of the Node.js callback pattern,
+    // where callbacks are in the form of function(err, result).
+    // Wrapping as thenable so make it support promise functionality.
     return new Promise((resolve, reject) => {
-      HTTP.request(feedurl)
-      .then(res => res.body.read())
-      .then(body => {
-        parser.parseString(body, (error, result) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(result);
-          }
-        })
+    HTTP.request(feedurl)
+    .then(res => res.body.read())
+    .then(body => {
+      parser.parseString(body, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
       })
-      .catch(reason => reject(reason))
+    })
+    .catch(reason => reject(reason))
     });
   },
 };
