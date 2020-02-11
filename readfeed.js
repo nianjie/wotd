eac69b1d4e6654eb9139d12f2e91a1d6a4f20413
developer1 <nianjie@hotmail.co.jp>
2020-02-11 14:56:24 +0900

    eslint.

--- readfeed.js
+++ readfeed.js
@@ -9,18 +9,18 @@ const readFeed = {
     // where callbacks are in the form of function(err, result).
     // Wrapping as thenable so make it support promise functionality.
     return new Promise((resolve, reject) => {
-    HTTP.request(feedurl)
-    .then(res => res.body.read())
-    .then(body => {
-      parser.parseString(body, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      })
-    })
-    .catch(reason => reject(reason))
+      HTTP.request(feedurl)
+        .then(res => res.body.read())
+        .then((body) => {
+          parser.parseString(body, (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          });
+        })
+        .catch(reason => reject(reason));
     });
   },
 };
