4008fdb03af519f5302f7ea8fc7f223278eae5d1
nj <nianjie@hotmail.co.jp>
2020-12-17 20:03:10 +0900

    auentication error.

--- firebase.account.js
+++ firebase.account.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   project_id: process.env.FIRE_PID,
   private_key: process.env.FIRE_PKEY.replace(/\\n/g, '\n'), // It is vital to replace '\\n' with '\n' because those escaped newlines(i.e. \n) in the original private key text are encoded again reuslted as '\\n', which is definitely wrong taken as private key.
   client_email: process.env.FIRE_CEMAIL,
-  databaseURL: 'https://q8732.firebaseio.com',
+  databaseURL: process.env.FIRE_DBURL,
 };
 
 module.exports = serviceAccount;
