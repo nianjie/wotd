48f4e9bcc44ba171b6a58cd8c3adc5e0463daa01
nj <nianjie@hotmail.co.jp>
2020-12-18 20:11:28 +0900

    update corelib.

--- lib/dictionary/crypto.d.ts
+++ lib/dictionary/crypto.d.ts
@@ -1,1 +1,8 @@
-export * from 'crypto';
+export declare const crypto: {
+    /**
+     * A replacement for Nodejs.crypto#randomInt().
+     * @param min
+     * @param max
+     */
+    randomInt(min: number, max: number): number;
+};
