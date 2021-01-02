98dab3a4105cad75e2e6d453be9b2091a64fb805
nj <4769034+nianjie@users.noreply.github.com>
2021-01-02 16:26:04 +0900

    fix eslint errors.

--- server.js
+++ server.js
@@ -67,9 +67,9 @@ function wotdChronological(req) {
 
 async function wotdAlphabetical(req) {
   let words = req.pathInfo.split('/');
-  words = words.filter(w => w.length > 0);
+  words = words.filter((w) => w.length > 0);
   console.log(`requested words ${words}`);
-  const all = words.map(w => oxfordDictionary.getWord(w));
+  const all = words.map((w) => oxfordDictionary.getWord(w));
   const definitions = await Promise.all(all);
   return APPS.json(definitions);
 }
